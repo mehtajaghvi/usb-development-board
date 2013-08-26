8e6f9c0d307d5e220d43ea9e7b0ee94d1541a606
jaghvi <jaghvi11@gmail.com>
2013-08-26 14:57:24 +0530

    single sided PCB

--- sketches/digispark_lm35_receive/digispark_lm35_receive.ino
+++ sketches/digispark_lm35_receive/digispark_lm35_receive.ino
@@ -8,34 +8,31 @@ void setup() {
 }
 
 void returnTemp( int degree ){
+
   int readValue = analogRead(TEMP);
   float celsius;
-  float fahrenheit;
+//  float fahrenheit;
  
 celsius=readValue*(5.0/1024.0); //converting voltage to corresponding temp value
   celsius=celsius*100;//
- fahrenheit = (1.8 * celsius) + 32;  
-
+ //fahrenheit = (1.8 * celsius) + 32;  
   if ( degree == 1 ){
     DigiUSB.print(celsius);
     DigiUSB.println("C");
-    DigiUSB.println("\n");
-    DigiUSB.print(fahrenheit);
-    DigiUSB.println("F");
-    DigiUSB.println("\n");
-    DigiUSB.println("------------------");
+//    DigiUSB.println("\n");
+    //DigiUSB.print(fahrenheit);
+    //DigiUSB.println("F");
+    //DigiUSB.println("-----------");
     DigiUSB.delay(10);
  
 }
   }
   
 
-
-
-
 void loop() {
   DigiUSB.refresh();
 returnTemp(1);
- DigiUSB.delay(500);
+
+ DigiUSB.delay(2900);
 
 }
